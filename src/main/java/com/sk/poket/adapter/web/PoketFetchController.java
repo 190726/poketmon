b492bd78a7b3b897137fb2bb0bb7fbc6463c4d9c
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-04-13 05:38:57 +0900

    poketFechController rest api url 수정

--- src/main/java/com/sk/poket/adapter/web/PoketFetchController.java
+++ src/main/java/com/sk/poket/adapter/web/PoketFetchController.java
@@ -2,15 +2,17 @@ package com.sk.poket.adapter.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController 
+@RestController
+@RequestMapping("/poket")
 public class PoketFetchController {
 	
-	@GetMapping("/find/{no}")
+	@GetMapping("/9g/{no}")
 	public String findBy(@PathVariable String no) {
 		log.info(no);
 		return "뜨아거";
