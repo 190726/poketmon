ff08dc26aa9fd0946ec5725ea625b4ee141c92bc
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-04-16 07:08:56 +0900

    html model pass as poketmons stub list

--- src/main/java/com/sk/poket/adapter/data/PoketSampleStub.java
+++ src/main/java/com/sk/poket/adapter/data/PoketSampleStub.java
@@ -1,5 +1,17 @@
 package com.sk.poket.adapter.data;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.sk.poket.domain.Poketmon;
+
 public class PoketSampleStub {
 
+	public static List<Poketmon> poketmons(){
+		Poketmon p1 = new Poketmon(1L, "나오하", "001.png");
+		Poketmon p2 = new Poketmon(2L, "나로테", "002.png");
+		Poketmon p3 = new Poketmon(3L, "마스카나", "003.png");
+		Poketmon p4 = new Poketmon(4L, "뜨아거", "004.png");
+		return Arrays.asList(p1, p2, p3, p4);
+	}
 }
--- src/main/java/com/sk/poket/adapter/web/PoketWebController.java
+++ src/main/java/com/sk/poket/adapter/web/PoketWebController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sk.poket.adapter.data.PoketSampleStub;
 import com.sk.poket.domain.Poketmon;
 
 @Controller
@@ -13,14 +14,7 @@ public class PoketWebController {
 	
 	@GetMapping("/poket")
 	public String poket(Model model) {
-		Poketmon p1 = new Poketmon(1L, "나오하", "001.png");
-		Poketmon p2 = new Poketmon(2L, "나로테", "002.png");
-		Poketmon p3 = new Poketmon(3L, "마스카나", "003.png");
-		Poketmon p4 = new Poketmon(4L, "뜨아거", "004.png");
-		model.addAttribute("poket1", p1);
-		model.addAttribute("poket2", p2);
-		model.addAttribute("poket3", p3);
-		model.addAttribute("poket4", p4);
+		model.addAttribute("poketmons", PoketSampleStub.poketmons());
 		return "poket";
 	}
 }
